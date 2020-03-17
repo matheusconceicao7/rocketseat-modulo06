85f3e7095c725ca5da5eaee30a65594a0ccaabc7
Matheus <matheuscosta2006@gmail.com>
2020-03-16 21:01:44 -0300

    Add loadMore function

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import api from '../../services/api';
 import {
+  LoadingIndicator,
   Container,
   Header,
   Avatar,
@@ -32,21 +33,37 @@ export default class User extends Component {
     super(props);
     this.state = {
       stars: [],
+      loading: true,
+      page: 1,
     };
   }
 
   async componentDidMount() {
+    return this.load();
+  }
+
+  load = async (page = 1) => {
     const { navigation } = this.props;
+    const { stars } = this.state;
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred`);
+    const response = await api.get(`/users/${user.login}/starred?page=${page}`);
+    this.setState({
+      loading: false,
+      page,
+      stars: page > 1 ? [...stars, ...response.data] : response.data,
+    });
+  };
 
-    this.setState({ stars: response.data });
-  }
+  loadMore = async () => {
+    const { page } = this.state;
+    const nextPage = page + 1;
+    return this.load(nextPage);
+  };
 
   render() {
     const { navigation } = this.props;
-    const { stars } = this.state;
+    const { stars, loading } = this.state;
     const user = navigation.getParam('user');
     return (
       <Container>
@@ -55,21 +72,28 @@ export default class User extends Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({ item }) => {
-            return (
-              <Starred>
-                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-                <Info>
-                  <Title>{item.name}</Title>
-                  <Author>{item.owner.login}</Author>
-                </Info>
-              </Starred>
-            );
-          }}
-        />
+        {loading ? (
+          <LoadingIndicator />
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            loading={loading}
+            onEndReachedThreshold={0.2} // Carrega mais itens quando chegar em 20% do fim
+            onEndReached={this.loadMore} // Função que carrega mais itens
+            renderItem={({ item }) => {
+              return (
+                <Starred>
+                  <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                  <Info>
+                    <Title>{item.name}</Title>
+                    <Author>{item.owner.login}</Author>
+                  </Info>
+                </Starred>
+              );
+            }}
+          />
+        )}
       </Container>
     );
   }
--- src/pages/User/styles.js
+++ src/pages/User/styles.js
@@ -1,4 +1,13 @@
 import styled from 'styled-components/native';
+import { ActivityIndicator } from 'react-native';
+
+export const LoadingIndicator = styled(ActivityIndicator).attrs({
+  color: '#000',
+})`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const Container = styled.View`
   flex: 1;
